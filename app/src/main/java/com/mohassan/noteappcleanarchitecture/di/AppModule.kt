04f3faeb634aabfee19ef2967265ece816e3c7a1
Mohamed Hassan <mohahassanna933@gmail.com>
2023-09-13 16:29:14 +0200

    AddEditNoteViewModel

--- app/src/main/java/com/mohassan/noteappcleanarchitecture/di/AppModule.kt
+++ app/src/main/java/com/mohassan/noteappcleanarchitecture/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.mohassan.noteappcleanarchitecture.feature_note.data.repository.NoteRe
 import com.mohassan.noteappcleanarchitecture.feature_note.domain.repository.NoteRepository
 import com.mohassan.noteappcleanarchitecture.feature_note.domain.use_case.AddNote
 import com.mohassan.noteappcleanarchitecture.feature_note.domain.use_case.DeleteNote
+import com.mohassan.noteappcleanarchitecture.feature_note.domain.use_case.GetNote
 import com.mohassan.noteappcleanarchitecture.feature_note.domain.use_case.GetNotes
 import com.mohassan.noteappcleanarchitecture.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -42,7 +43,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
--- app/src/main/java/com/mohassan/noteappcleanarchitecture/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/mohassan/noteappcleanarchitecture/feature_note/domain/use_case/NoteUseCases.kt
@@ -3,5 +3,6 @@ package com.mohassan.noteappcleanarchitecture.feature_note.domain.use_case
 data class NoteUseCases(  // wrapper class for single operation
     val getNotes: GetNotes,
     val deleteNote: DeleteNote,
-    val addNote: AddNote
+    val addNote: AddNote,
+    val getNote: GetNote
 )
